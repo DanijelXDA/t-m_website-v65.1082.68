14c50e0d938b1d85f590d217e7a9d019c69873f3
danijelxda <danijelj2001.xda@gmail.com>
2018-04-22 13:36:09 +0200

    Return to the present

---
+++ oTimu/js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+
+  "use strict";
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+
+}(jQuery));
