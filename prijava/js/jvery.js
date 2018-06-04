44b4b13ce817c67923ec14069d5531ba0b214362
danijelxda <danijelj2001.xda@gmail.com>
2018-06-04 19:57:24 +0200

    Typo error fix

--- prijava/js/jvery.js
+++ prijava/js/jvery.js
@@ -1,11 +1,23 @@
     function validateForm() {
         var un = document.loginform.usr.value;
         var pw = document.loginform.pword.value;
-        var username = "webMaster"; 
-        var password = "P@ssw0rd";
-        if ((un == username) && (pw == password)) {
+		
+        var u1 = "d.jovanovic"; 
+        var p1 = "sifraJeSifra";
+		
+		var u2 = "p.matovic";
+		var p2 = "sifraJePasvord";
+		
+		var u3 = "s.maricic";
+		var p3 = "sifraNijeSifra";
+		
+		
+        if ((un == u1) && (pw == p1))
             return true;
-        }
+        else if ((un == u2) && (pw == p2))
+			return true;
+		else if ((un == u3) && (pw == p3))
+			return true;
         else {
 			var x = document.getElementById("myDIV");
             x.style.display = "block";
