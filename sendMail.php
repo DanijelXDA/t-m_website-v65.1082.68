a1e4432c8a673f7bb037e33d394259dfa46d153a
danijelxda <danijelj2001.xda@gmail.com>
2018-08-16 17:55:43 +0200

    Recode and bug fixes with send mail script

--- sendMail.php
+++ sendMail.php
@@ -2,7 +2,7 @@
 	$sadrzajMejla = 'Име: '. $_POST['ime'] . "" . PHP_EOL . 'Е-адреса: '. $_POST['ime'] . "" . PHP_EOL . $_POST['poruka'];
 	$to      = 'danijelj2001.xda@gmail.com';
 	$subject = 'Питање/Сугестија | Техничка школа';
-	$message = $sadrzajMejla;
+	$message = wordwrap($message, 70, "\r\n");
 	$headers = 'From: no-reply@tehnickaskola.edu.rs' . "\r\n" .
 		'Reply-To: no-reply@tehnickaskola.edu.rs' . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
