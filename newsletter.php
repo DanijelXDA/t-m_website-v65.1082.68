8dcb4d7d7f89db50da2985bbe5a32d573ba580cc
danijelxda <danijelj2001.xda@gmail.com>
2018-08-18 13:41:23 +0200

    Init Newletter script

---
+++ newsletter.php
@@ -0,0 +1,14 @@
+<?php 
+	$name = $_POST['ime'];
+	$email = $_POST['email'];
+	$message = $_POST['poruka'];
+	$formcontent="Име: $name \nE-адреса: $email\nПорука: $message\n";
+	$recipient = "danijelj2001.xda@gmail.com";
+	$subject = "Питање/Сугестија | Техничка школа";
+	$headers = 'From: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+		'Reply-To: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	mail($recipient, $subject, $formcontent, $headers) or die("Greska!");
+	header( 'Location: success.html' );
+	header('Refresh: 10; URL=https://tehnickaskola.edu.rs/');
+?>
