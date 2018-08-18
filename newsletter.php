40d38e196efcc1f18c890dc258c55bb752350771
danijelxda <danijelj2001.xda@gmail.com>
2018-08-18 14:10:10 +0200

    Fix Newletter script

--- newsletter.php
+++ newsletter.php
@@ -1,12 +1,10 @@
 <?php 
-	$name = $_POST['ime'];
-	$email = $_POST['email'];
-	$message = $_POST['poruka'];
-	$formcontent="Име: $name \nE-адреса: $email\nПорука: $message\n";
-	$recipient = "danijelj2001.xda@gmail.com";
-	$subject = "Питање/Сугестија | Техничка школа";
-	$headers = 'From: no-reply@tehnickaskola.edu.rs' . "\r\n" .
-		'Reply-To: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+	$email = $_POST['mejl'];
+	$formcontent="Успешно си се пријавио на Newsletter Техничке школе.\nХвала на пријави!";
+	$recipient = $email;
+	$subject = "Претплата на Newsletter | Техничка школа";
+	$headers = 'From: no-reply-newsletter@tehnickaskola.edu.rs' . "\r\n" .
+		'Reply-To: no-reply-newsletter@tehnickaskola.edu.rs' . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
 	mail($recipient, $subject, $formcontent, $headers) or die("Greska!");
 	header( 'Location: success.html' );
