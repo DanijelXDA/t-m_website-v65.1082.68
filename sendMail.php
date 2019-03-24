1ef01db25020caa1ced80112de78dca22ad72aef
danijelxda <danijelj2001.xda@gmail.com>
2019-03-24 19:21:10 +0100

    Add some PHP

---
+++ sendMail.php
@@ -0,0 +1,13 @@
+<?php 
+	$name = $_POST['ime'];
+	$email = $_POST['email'];
+	$message = $_POST['poruka'];
+	$formcontent="Име: $name \nE-адреса: $email\nПорука: $message\n";
+	$recipient = "danijelji10a2016@gmail.com";
+	$subject = "Питање/Сугестија | Техничка школа";
+	$headers = 'From: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+		'Reply-To: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	mail($recipient, $subject, $formcontent, $headers) or die("Greska!");
+	header( 'Location: pretplata.html' );
+?>
